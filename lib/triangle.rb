c3ac707ed793a4102b95364d4d9fdd10769f4421
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-10-31 02:56:38 +0300

    instance method defination

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,22 @@
 class Triangle
   # write code here
+  #accessor macro
+  attr_reader :a, :b, :c
+  #method initialization
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+  #instance method definition
+  def kind
+    validate_triangle
+    if a == b && b == c
+      :equilateral
+    elsif a == b || b == c || a == c
+      :isosceles
+    else
+      :scalene
+    end
+  end
 end
